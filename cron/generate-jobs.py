3113af77b582c984df2af343448ac7b49c8ce929
Andrew Godwin <andrew@aeracode.org>
2016-08-26 19:41:51 -0700

    Say name of env var job is made from

--- cron/generate-jobs.py
+++ cron/generate-jobs.py
@@ -9,6 +9,6 @@ for key, value in os.environ.items():
         cron_path = "/var/spool/cron/crontabs/root"
         with open(cron_path, "a") as fh:
             fh.write(value)
-        print("Wrote to %s: %s" % (cron_path, value))
+        print("Wrote %s to %s: %s" % (key, cron_path, value))
 
 print("Initialisation finished.")
