f3a5e093eafaac1ad0e0f15361ec7732a172ab74
Andrew Godwin <andrew@aeracode.org>
2017-04-12 21:21:09 -0700

    Slight improvements

--- get_iplayer/run-pvr.py
+++ get_iplayer/run-pvr.py
@@ -6,6 +6,9 @@ import time
 import datetime
 import subprocess
 
+print("Booting PVR script")
+sys.stdout.flush()
+
 # Get target
 binary = "/srv/get_iplayer/get_iplayer"
 target = os.environ.get("PVRDIR", "/srv/pvr")
@@ -23,8 +26,9 @@ subprocess.check_call(["rm", "-rf", profile_pvr_dir])
 subprocess.check_call(["rm", "-rf", os.path.join(profile_dir, "pvr_lock")])
 
 # Re-add searches
-for name, value in searches.items():
+for name, value in sorted(searches.items()):
     print("Adding PVR search %s: %s" % (name, value), file=sys.stderr)
+    sys.stderr.flush()
     subprocess.check_call([
         binary,
         "--profile-dir=%s" % profile_dir,
@@ -36,8 +40,10 @@ for name, value in searches.items():
 # PVR loop
 while True:
     # Run PVR downloads
-    print("Starting loop at %s" % datetime.datetime.utcnow(), file=sys.stderr)
-    print("Running PVR download", file=sys.stderr)
+    print("Starting loop at %s" % datetime.datetime.utcnow(), file=sys.stdout)
+    print("Running PVR download", file=sys.stdout)
+    sys.stdout.flush()
+    subprocess.call(["bash", "-c", "date"])
     subprocess.call([
         binary,
         "--profile-dir=%s" % profile_dir,
@@ -45,18 +51,21 @@ while True:
         "--modes=best",
         '--fileprefix=<nameshort><.senum><.episodeshort>',
         "--overwrite",
+        "--expiry=3600",
         "-o", target,
     ])
     # Delete old PVR recordings
-    print("Deleting old recordings", file=sys.stderr)
+    print("Deleting old recordings", file=sys.stdout)
+    sys.stdout.flush()
     subprocess.check_call([
         "find",
         target,
         "-type", "f",
-        "-mtime", "+14",
+        "-mtime", "+3",
         "-iname", "*.mp4",
         "-delete",
     ])
     # Sleeeeeep
-    print("Sleeping for an hour", file=sys.stderr)
+    print("Sleeping for an hour", file=sys.stdout)
+    sys.stdout.flush()
     time.sleep(3600)
